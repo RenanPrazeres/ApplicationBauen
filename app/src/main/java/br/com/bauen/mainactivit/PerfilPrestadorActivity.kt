cad57126a7d16f9f2d9d09e9885ab1d7e525fc91
RENAN SILVA PRAZERES <20272594@cfp127aluno.local>
2021-10-15 10:12:31 -0300

    Perfil Prestador com Avaliação

--- app/src/main/java/br/com/bauen/mainactivit/PerfilPrestadorActivity.kt
+++ app/src/main/java/br/com/bauen/mainactivit/PerfilPrestadorActivity.kt
@@ -2,10 +2,21 @@ package br.com.bauen.mainactivit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class PerfilPrestadorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.perfil_prestador)
+
+
+
+//        rb_ratingBar.rating = 2.5f
+//        rb_ratingBar = stepSize = .5f
+//
+//        rb_rating.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+//            Toast.makeText( this, "Rating: $rating", Toast.LENGTH_SHORT).show()
+//
+//        }
     }
 }
