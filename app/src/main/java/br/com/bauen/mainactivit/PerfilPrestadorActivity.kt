b54d323269b491a92507c8776186086b090b630c
RENAN SILVA PRAZERES <20272594@cfp127aluno.local>
2021-10-21 11:05:34 -0300

    Atribundo função ao botao ENTRAR_EM_CONTATO Concluido

--- app/src/main/java/br/com/bauen/mainactivit/PerfilPrestadorActivity.kt
+++ app/src/main/java/br/com/bauen/mainactivit/PerfilPrestadorActivity.kt
@@ -1,14 +1,33 @@
 package br.com.bauen.mainactivit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class PerfilPrestadorActivity : AppCompatActivity() {
+
+    lateinit var btncontato : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.perfil_prestador)
 
+        btncontato = findViewById(R.id.btn_contato)
+
+
+        //BOTAO PARA ENTRAR EM CONTATO, DIRECIONANDO PARA TELA DE CHAT
+        btncontato.setOnClickListener {
+            val chat = Intent(this, ChatActivity::class.java)
+            startActivity(chat)
+
+        }
+
+    }
+}
 
 
 //        rb_ratingBar.rating = 2.5f
@@ -16,7 +35,5 @@ class PerfilPrestadorActivity : AppCompatActivity() {
 //
 //        rb_rating.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
 //            Toast.makeText( this, "Rating: $rating", Toast.LENGTH_SHORT).show()
-//
+
 //        }
-    }
-}
