5424b1767b78001c3daa993e6276614cd95687fc
jeanjhc <henrique_carvalho04@hotmail.com>
2021-10-27 11:07:07 -0300

    criação menu 'nav'e chat

--- app/src/main/java/br/com/bauen/mainactivit/ChatActivity.kt
+++ app/src/main/java/br/com/bauen/mainactivit/ChatActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        setContentView(R.layout.activity_historico_chat)
     }
 }
---
+++ app/src/main/java/br/com/bauen/mainactivit/MensagensRecentesActivity.kt
@@ -0,0 +1,29 @@
+package br.com.bauen.mainactivit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MensagensRecentesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mensagens_recentes)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId){
+            R.id.menu_voltar ->{
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+}
