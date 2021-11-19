16a74934419c1b50dc9c06379ffed89653c7ace8
BEATRIZ FREITAS SANTOS <20272492@cfp127aluno.local>
2021-11-19 11:25:15 -0300

    inicio dialog cadastro

---
+++ app/src/main/java/br/com/bauen/mainactivit/ultis/DialogCadastro.kt
@@ -0,0 +1,10 @@
+package br.com.bauen.mainactivit.ultis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DialogCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
