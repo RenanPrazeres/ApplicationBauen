dbdb0f46f34080e91bf3d453312124be4690e303
jeanjhc <henrique_carvalho04@hotmail.com>
2021-11-19 11:10:41 -0300

    consumindo api do chat

--- app/src/main/java/br/com/bauen/mainactivit/MainActivity.kt
+++ app/src/main/java/br/com/bauen/mainactivit/MainActivity.kt
@@ -1,19 +1,23 @@
 package br.com.bauen.mainactivit
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_historico_chat)
 
-        SocketHandler.setSocket()
+        val buttonSendMsg = findViewById<ImageButton>(R.id.button_send_msg)
 
-        val mSocket=SocketHandler.getSocket()
-
-        mSocket.connect()
+        buttonSendMsg.setOnClickListener {
+            val sendMsg = Intent(this, ChatActivityLog::class.java)
+            startActivity(sendMsg)
+        }
     }
 }
